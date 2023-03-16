09bb09982c60878d2e3653ce4366df7e06a542bb
DenViko <DenViko@yahoo.com>
2023-03-16 15:29:27 +0500

    HW

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public void main(String[] args) {
+        Car car = new Car();
+        Car car2 = new Car();
+        car.modelName = "car1";
+        car2.modelName = "car2";
+        car.wheelsCount = 4;
+        car2.wheelsCount = 4;
+
+        Truck truck = new Truck("truck1",6);
+        Truck truck2 = new Truck("truck2",8);
+        String modelName = truck.getModelName();
+        String ModelName2 = truck2.getModelName();
+        int WheelsCount = truck.getWheelsCount();
+        int WheelsCount2 = truck2.getWheelsCount();
+
+
+        Bicycle bicycle = new Bicycle();
+        Bicycle bicycle2 = new Bicycle();
+        bicycle.modelName = "bicycle1";
+        bicycle2.modelName = "bicycle2";
+        bicycle.wheelsCount = 2;
+        bicycle2.wheelsCount = 2;
+
+        ServiceStation station = new ServiceStation();
+        station.check(car, null, null);
+        station.check(car2, null, null);
+        station.check(null, bicycle, null);
+        station.check(null, bicycle2, null);
+        station.check(null, null, truck);
+        station.check(null, null, truck2);
     }
 }
+
+
---
+++ src/Truck.java
@@ -0,0 +1,38 @@
+public class Truck {
+    private String modelName;
+    private int wheelsCount;
+
+    public Truck(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+
+    public void checkTrailer() {
+        System.out.println("Проверяем прицеп");
+    }
+
+    @Override
+    public String toString() {
+        return "Truck{" +
+                "modelName='" + modelName + '\'' +
+                ", wheelsCount=" + wheelsCount +
+                '}';
+    }
+}
+
